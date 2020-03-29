325b0e517f5f9e4ef20ada950d133b95aa259949
vaibhavsharmakv <vaibhavsharmakv@gmail.com>
2020-03-28 20:32:31 -0600

    just new comments for future work

--- DrawingBoard/Src/Js/functions.js
+++ DrawingBoard/Src/Js/functions.js
@@ -3,6 +3,7 @@ deselectVar.addEventListener("click", deselectPressed);
 
 function deselectPressed()
 {
+	// change the icon to same as the cursor == no-drop
     pencil = false;
     eraser = false;
     document.body.style.cursor = "no-drop";
